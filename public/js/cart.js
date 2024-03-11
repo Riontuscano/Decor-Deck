21dce60472df3bffc2001a8c36e7640974e81bf2
Rion Tuscano <riontuscano@gmail.com>
2024-03-11 21:23:03 +0530

    go

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,7 +1,7 @@
 const payBtn = document.querySelector('.cart-btn');
 payBtn.addEventListener('click', () => {
   console.log(JSON.parse(localStorage.getItem('cartItems')));
-  fetch('/stripe-checkout', { // Update the base URL
+  fetch('https://localhost:3000/stripe-checkout', { // Update the base URL
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
